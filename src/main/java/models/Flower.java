4da004b7549cb54a4029b3d9098abd44fc2f1e17
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-06 15:00:56 -0400

    model fields initialized

--- src/main/java/models/Flower.java
+++ src/main/java/models/Flower.java
@@ -1,4 +1,70 @@
 package models;
 
 public class Flower {
+    private int id;
+    private int qty;
+    private String color;
+    private String flowerType;
+    private boolean isWrapped;
+    private double price;
+    public Flower() {
+        this.id = 0;
+        this.color = "N/A";
+        this.flowerType = "N/A";
+        this.price = 0;
+    }
+    public Flower(int id, String color, String flowerType, double price) {
+        this.id = id;
+        this.color = color;
+        this.flowerType = flowerType;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFlowerType() {
+        return flowerType;
+    }
+
+    public void setFlowerType(String flowerType) {
+        this.flowerType = flowerType;
+    }
+
+    public boolean isWrapped() {
+        return isWrapped;
+    }
+
+    public void setWrapped(boolean wrapped) {
+        isWrapped = wrapped;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/models/Painting.java
+++ src/main/java/models/Painting.java
@@ -1,4 +1,76 @@
 package models;
 
 public class Painting {
+    private String artistName;
+    private String paintingName;
+    private double height;
+    private double width;
+    private double price;
+    private int id;
+
+    public Painting() {
+        this.id = 0;
+        this.artistName = "Artist Name";
+        this.paintingName = "Painting Name";
+        this.height = 0;
+        this.width = 0;
+        this.price = 0;
+    }
+
+    public Painting(String artistName, String paintingName, double height, double width, double price, int id) {
+        this.artistName = artistName;
+        this.paintingName = paintingName;
+        this.height = height;
+        this.width = width;
+        this.price = price;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getPaintingName() {
+        return paintingName;
+    }
+
+    public void setPaintingName(String paintingName) {
+        this.paintingName = paintingName;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
