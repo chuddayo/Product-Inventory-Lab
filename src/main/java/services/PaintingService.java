f64a862d95ce8f1d2a34fa5ce610e516e2346000
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-07 21:33:53 -0400

    service tests finished

--- src/main/java/services/PaintingService.java
+++ src/main/java/services/PaintingService.java
@@ -1,4 +1,17 @@
 package services;
 
+import models.Painting;
+
+import java.util.ArrayList;
+
 public class PaintingService {
+    private static int nextId = 1;  // (1)
+
+    private ArrayList<Painting> inventory = new ArrayList<>();  // (2)
+
+    public Painting create(String artistName, String paintingName, double height, double width, double price) {
+        Painting createdPainting = new Painting(artistName, paintingName, height, width, price, nextId++);
+        inventory.add(createdPainting);
+        return createdPainting;
+    }
 }
--- src/test/java/services/PaintingServiceTest.java
+++ src/test/java/services/PaintingServiceTest.java
@@ -1,4 +1,37 @@
 package services;
 
+import models.Flower;
+import models.Painting;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PaintingServiceTest {
+    @Test
+    public void createTest() {
+        // given
+        String artistName = "Smith";
+        String paintingName = "Genius";
+        double height = 14.32;
+        double width = 83.99;
+        double price = 14.49;
+
+        // when
+        PaintingService paintingService = new PaintingService();
+        Painting testPainting = paintingService.create(artistName, paintingName, height, width, price);
+
+        // then
+        int actualId = testPainting.getId();
+        String actualArtistName = testPainting.getArtistName();
+        String actualPaintingName = testPainting.getPaintingName();
+        double actualHeight = testPainting.getHeight();
+        double actualWidth = testPainting.getWidth();
+        double actualPrice = testPainting.getPrice();
+
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(artistName, actualArtistName);
+        Assertions.assertEquals(paintingName, actualPaintingName);
+        Assertions.assertEquals(height, actualHeight);
+        Assertions.assertEquals(width, actualWidth);
+        Assertions.assertEquals(price, actualPrice);
+    }
 }
