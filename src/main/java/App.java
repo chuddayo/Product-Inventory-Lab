021d01fe2ee761f6b86f26b39892289d113cdaeb
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-07 22:07:01 -0400

    created main menu

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,9 @@ import io.Console;
 import services.FlowerService;
 import services.PaintingService;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     private FlowerService flowerService = new FlowerService();
     private PaintingService paintingService = new PaintingService();
@@ -13,5 +16,29 @@ public class App {
 
     public void init() {
         Console.printWelcome();
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        do {
+            Console.printMenu();
+            input = in.next().toLowerCase();
+            switch (input) {
+                case "a":
+                    Console.addProduct();
+                    break;
+                case "s":
+                    Console.printExisting();
+                    break;
+                case "u":
+                    Console.printExisting();
+                    Console.updateProducts();
+                    break;
+                case "d":
+                    Console.printExisting();
+                    Console.deleteProducts();
+                    break;
+            }
+
+        } while (!input.equals("x"));
     }
 }
--- src/main/java/io/Console.java
+++ src/main/java/io/Console.java
@@ -3,10 +3,33 @@ package io;
 public class Console {
     public static void printWelcome(){
         System.out.println("" +
-                "**************************************************" +
-                "***           Welcome and Bienvenue            ***" +
-                "***                    to                      ***" +
-                "***          ZipCo Inventory Manager           ***" +
+                "**************************************************\n" +
+                "***                 Welcome to                 ***\n" +
+                "***          ZipCo Inventory Manager           ***\n" +
                 "**************************************************");
+    }
+    public static void printMenu(){
+        System.out.println(
+                "\nMain Menu\n" +
+                "(a) - add a product\n" +
+                "(s) - see existing products\n" +
+                "(u) - udpate existing products\n" +
+                "(d) - delete existing products\n" +
+                "(x) - exit the menu"
+        );
+    }
+    public static void addProduct(){
+
+    }
+    public static void printExisting(){
+
+    }
+
+    public static void updateProducts(){
+
+    }
+
+    public static void deleteProducts(){
+
     }
 }
